721bed7ebd5acc6b98eb09c5d3453d3a26bd1a8e
Khoi <daongocanhkhoi@gmail.com>
2022-10-03 19:16:19 +0700

    complete

---
+++ js/jquery.js
@@ -0,0 +1,8 @@
+// $(document).ready (function() {
+//     $("#gift").click(function(){
+//         alert(123)
+//         $("#theDen").css ({
+             
+//         });
+//     });
+// });
--- js/main.js
+++ js/main.js
@@ -1,12 +1,19 @@
+const { get } = require("jquery");
+
 function getEle(id) {
     return document.getElementById(id);
 }
 
 function batDen() { 
-  
     var den = getEle("theDen");
         den.src = "./img/mechiLien.jpg";
         den.style.margin = '20px 0';
         var phaoHoa = getEle("phaohoa");
         phaoHoa.className = 'pyro';
+        batNhac();
+}
+
+function batNhac () {
+    var button = getEle("sound");
+    button.play();
 }
